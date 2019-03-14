cc867ccea08339a6b99878aad1918ada354482c7
Quentin Sander <02quentin.sander@gulllakecs.org>
2019-03-14 07:21:21 -0400

    Add Int

--- Final Project.py
+++ Final Project.py
@@ -1,11 +1,113 @@
-#3.7.19
-#Quentin Sander
-speed(12)
-for i in range(180):
- left(2)
- forward(5)
-forward(10)
 
-for i in range(90):
- right(2)
- forward(5)
+
+#3.7.19
+
+#Quentin Sander
+
+sum = 0
+
+for i in range(5):
+    enter_a_num = int(input("your fastest throw for evrey year:"))
+    sum = sum + enter_a_num
+
+print("")
+
+print("The Sum Of Your Number Is: " + str(sum))
+
+while True:
+    speed_of_ball = int(input("speed of ball for your age:"))
+    if speed_of_ball > 60:
+        print("That is to fast " + "\n")
+    elif speed_of_ball < 55:
+        print("That is way to slow", "\n")
+    else:
+        print("That is perfect for your age", "\n")
+        break
+
+print("Hooray, You Found The Perfect Bike Frame Size")
+
+try:
+    my_num = int(input('Enter an integer: '))
+    print('Your number:', my_num)
+except ValueError:
+    print('\n''That was not an integer, (: ')
+
+name = input('Enter Name:')
+
+age = -1
+
+try:
+    age = int(input('Enter Your Age:'))
+
+except ValueError:
+    print('\n''That was not an integer for your age')
+
+print('\n''Name:', name)
+print('Age:', age)
+
+
+my_number = 10
+
+print ("Guess a number between 1 and 10")
+print("")
+
+guess = int(input("Enter a guess:"))
+
+while guess != my_number:
+    print("")
+    print ("Wrong, guess again")
+    guess = int(input("Enter a guess: "))
+
+print("")
+print ("Good jod, you got it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+
+
+x = 20
+
+while x > 5:
+    print(x)
+    x = x - 2
+
+
+    def print_number(x):
+     print(x)
+print('\n', x)
+
+print_number(13)
+print_number(23)
+
+
+def name_and_age(name, age):
+ print('\n', 'Hi, my name is', name, 'and I am', str(age), 'years old!')
+
+
+name_and_age('Mike', 33)
+name_and_age('Zane', 18)
+
+
+
+def print_two_number(x, y=20):
+ print('First number:', x)
+ print('Second number: ' + str(y))
+
+
+print_two_number(34, 45)
+print_two_number(78)
+
+
+
+def print_sum(x, y):
+ print(x + y)
+
+
+print_sum(46, 62)
+
+
+
+def print_multiple_times(string, times):
+ for i in range(times):
+  print(string)
+
+
+print_multiple_times('Hey there Computer Scientist', 1)
